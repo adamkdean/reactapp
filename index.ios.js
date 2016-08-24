fb4074b0058242780f506cfa18ce261ad1be9d68
Adam K Dean <adamkdean@googlemail.com>
2016-08-24 12:44:08 +0100

    add rudimentary gridview prototype

--- index.ios.js
+++ index.ios.js
@@ -1,6 +1,30 @@
-import React from 'react'
-import { AppRegistry } from 'react-native'
+'use strict'
 
-import CameraTest from './CameraTest'
+import React, { Component } from 'react'
+import { AppRegistry, StyleSheet, View, Text } from 'react-native'
+import GridView from './GridView'
 
-AppRegistry.registerComponent('reactapp', () => CameraTest)
+class ReactApp extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      data: []
+    }
+
+    for (let i = 0; i < 100; i++) {
+      this.state.data.push({
+        id: i,
+        uri: 'https://facebook.github.io/react/img/logo_og.png'
+      })
+    }    
+  }
+
+  render() {
+    return (
+      <GridView>{this.state.data}</GridView>
+    )
+  }
+}
+
+AppRegistry.registerComponent('reactapp', () => ReactApp)
