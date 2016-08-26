45262e9c8a42cdd2c4bc39cd1416da82b7ec2877
Adam K Dean <adamkdean@googlemail.com>
2016-08-26 03:46:21 +0100

    move gridview + add fill gridview with real data

---
+++ components/GridView.js
@@ -0,0 +1,100 @@
+import React, { Component } from 'react'
+import {
+  AppRegistry,
+  ListView,
+  Image,
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+  View
+} from 'react-native'
+
+export default class GridView extends Component {
+  constructor(props) {
+    super(props)
+    console.log('constructor', props)
+    this._setInitialState()
+    this._bindFunctions()
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const data = nextProps.children || []
+    const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 })
+    const dataSource = ds.cloneWithRows(data)
+    this.setState({ ...this.state, dataSource })
+  }
+
+  render() {
+    return (
+      <ListView
+        contentContainerStyle={styles.list}
+        dataSource={this.state.dataSource}
+        renderRow={this._renderRow}
+        initialListSize={30}
+      />
+    )
+  }
+
+  _setInitialState() {
+    const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 })
+    this.state = {
+      dataSource: ds
+    }
+  }
+
+  _bindFunctions() {
+    this._renderRow = this._renderRow.bind(this)
+    this._selectItem = this._selectItem.bind(this)
+  }
+
+  _renderRow (rowData, sectionId, rowId) {
+    const imgSource = rowData.uri
+      ? { uri: rowData.uri }
+      : rowData.data
+
+    console.log('rowData', rowData)
+    console.log('imgSource', imgSource)
+    console.log()
+
+    return (
+      <TouchableHighlight
+        style={styles.row}
+        onPress={() => this._selectItem(rowData)}
+        underlayColor='rgba(0,0,0,0)'>
+        <View>
+          <Image style={styles.thumb} source={imgSource} />
+        </View>
+      </TouchableHighlight>
+    )
+  }
+
+  _selectItem (item) {
+    if (typeof item.onSelect === 'function') {
+      item.onSelect()
+    } else {
+      // do something with item
+      console.log('item selected', item)
+    }
+  }
+}
+
+const styles = StyleSheet.create({
+  list: {
+    justifyContent: 'flex-start',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    padding: 8,
+  },
+  row: {
+    backgroundColor: '#aaaaaa',
+    justifyContent: 'center',
+    margin: 2,
+    width: 72,
+    height: 72,
+    alignItems: 'center'
+  },
+  thumb: {
+    width: 72,
+    height: 72
+  }
+})
--- index.ios.js
+++ index.ios.js
@@ -1,12 +1,81 @@
 import React, { Component } from 'react'
-import { AppRegistry } from 'react-native'
-
-import CameraRollExample from './examples/CameraRollExample'
+import {
+  AppRegistry,
+  CameraRoll,
+  Image,
+  Navigator,
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+  View
+} from 'react-native'
+import GridView from './components/GridView'
 
 class ReactApp extends Component {
+  constructor(props) {
+    super(props)
+    this._setInitialState()
+    this._bindFunctions()
+  }
+
+  componentDidMount() {
+    CameraRoll
+      .getPhotos({ first: 20, groupTypes: 'All' })
+      .then(this._storeImages)
+      .catch(this._logError)
+  }
+
   render() {
-    return <CameraRollExample />
+    console.log(`rendering with ${this.state.images.length} images`, this.state.images)
+    return (
+      <View style={styles.container}>
+        <GridView key='gridview_01' name='gridview_01' count={this.state.images.length}>{this.state.images}</GridView>
+      </View>
+    )
+  }
+
+  _setInitialState() {
+    this.state = {
+      images: []
+    }
+  }
+
+  _bindFunctions() {
+    this._storeImages = this._storeImages.bind(this)
+    this._logError = this._logError.bind(this)
+  }
+
+  _storeImages(data) {
+    const assets = data.edges
+    const images = assets.map(asset => asset.node.image)
+
+    // add the camera placeholder
+    images.unshift({
+      data: require('./assets/camera.png'),
+      onSelect: () => {
+        console.log('delegate !!')
+      }
+    })
+
+    this.setState({ ...this.state, images })
+  }
+
+  _logError(error) {
+    console.log('Error', error)
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: '#eeeeee',
+    marginTop: 22
+  },
+  image: {
+    width: 100,
+    height: 100
+  }
+})
 
 AppRegistry.registerComponent('reactapp', () => ReactApp)
